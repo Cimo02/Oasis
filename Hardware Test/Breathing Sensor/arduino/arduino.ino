ea68e90beafcefba82142bf7affe1a92c69f14ba
Alex Pasieka <alexspasieka@gmail.com>
2020-02-20 10:19:26 -0500

    breathing sensor functionality

---
+++ Hardware Test/Breathing Sensor/arduino/arduino.ino
@@ -0,0 +1,60 @@
+/******************************************************************************
+Force_Sensitive_Resistor_Example.ino
+Example sketch for SparkFun's force sensitive resistors
+  (https://www.sparkfun.com/products/9375)
+Jim Lindblom @ SparkFun Electronics
+April 28, 2016
+
+Create a voltage divider circuit combining an FSR with a 3.3k resistor.
+- The resistor should connect from A0 to GND.
+- The FSR should connect from A0 to 3.3V
+As the resistance of the FSR decreases (meaning an increase in pressure), the
+voltage at A0 should increase.
+
+Development environment specifics:
+Arduino 1.6.7
+******************************************************************************/
+const int FSR_PIN = A0; // Pin connected to FSR/resistor divider
+
+// Measure the voltage at 5V and resistance of your 3.3k resistor, and enter
+// their value's below:
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 3230.0; // Measured resistance of 3.3k resistor
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(FSR_PIN, INPUT);
+}
+
+void loop() 
+{
+  int fsrADC = analogRead(FSR_PIN);
+  // If the FSR has no pressure, the resistance will be
+  // near infinite. So the voltage should be near 0.
+  if (fsrADC != 0) // If the analog reading is non-zero
+  {
+    // Use ADC reading to calculate voltage:
+    float fsrV = fsrADC * VCC / 1023.0;
+    // Use voltage and static resistor value to 
+    // calculate FSR resistance:
+    float fsrR = R_DIV * (VCC / fsrV - 1.0);
+    Serial.println("Resistance: " + String(fsrR) + " ohms");
+    // Guesstimate force based on slopes in figure 3 of
+    // FSR datasheet:
+    float force;
+    float fsrG = 1.0 / fsrR; // Calculate conductance
+    // Break parabolic curve down into two linear slopes:
+    if (fsrR <= 600) 
+      force = (fsrG - 0.00075) / 0.00000032639;
+    else
+      force =  fsrG / 0.000000642857;
+    Serial.println(force);
+
+    delay(500);
+  }
+  else
+  {
+    // No pressure detected
+  }
+}
--- PerlinP5js/PerlinP5js.js
+++ PerlinP5js/PerlinP5js.js
@@ -1,94 +1,89 @@
-var particles_a = [];
-var particles_b = [];
-var particles_c = [];
-var nums = 200;
-var noiseScale = 800;
+let particles_a = [];
+let particles_b = [];
+let particles_c = [];
 
-var firstLoop = true;
+const numParticles = 200;
+const noiseScale = 800;
 
-function setup(){
-  // window settings
+let backgroundColor;
+let lineColor;
+let textColor;
+
+function setup() {
+  backgroundColor = color(230, 233, 235);
+  lineColor = color(132, 164, 246);
+  textColor = color(132, 164, 246);
+
   createCanvas(windowWidth, windowHeight);
-  background(230, 233, 235);
-  
-  // setup GUI font and prepare to write the first message
+  background(backgroundColor);
+
   textFont('DM Sans');
-  strokeWeight(2);
   textSize(40);
   textAlign(CENTER);
-  firstLoop = true;
-  
-  // create buttons
-  
-  // create particles
-  for(var i = 0; i < nums; i++){
-    particles_a[i] = new Particle(random(0, width),random(0,height));
-    particles_b[i] = new Particle(random(0, width),random(0,height));
-    particles_c[i] = new Particle(random(0, width),random(0,height));
+  strokeWeight(2);
+  drawGUI();
+
+  for (let i = 0; i < numParticles; i++) {
+    particles_a[i] = new Particle(random(0, windowWidth), random(0, windowHeight));
+    particles_b[i] = new Particle(random(0, windowWidth), random(0, windowHeight));
+    particles_c[i] = new Particle(random(0, windowWidth), random(0, windowHeight));
   }
+
+  noStroke();
+  smooth();
 }
 
-// update function
-function draw(){
-  smooth();
-  fill(132,164,246,255);
-  
-  if (firstLoop) {
-    drawGUI(); 
-  }
-  
-  noStroke();
-  
-  for(var i = 0; i < nums; i++){
-    var radius = map(i,0,nums,1,2);
-    var alpha = map(i,0,nums,0,250);
+function draw() {
+  for (let i = 0; i < numParticles; i++) {
+    let radius = map(i, 0, numParticles, 1, 2);
 
-    fill(132,164,246,10);
+    lineColor.setAlpha(10);
+    fill(lineColor);
     particles_a[i].move();
     particles_a[i].display(radius);
     particles_a[i].checkEdge();
 
-    fill(132,164,246,30);
+    lineColor.setAlpha(30);
+    fill(lineColor);
     particles_b[i].move();
     particles_b[i].display(radius);
     particles_b[i].checkEdge();
 
-    fill(132,164,246,50);
+    lineColor.setAlpha(50);
+    fill(lineColor);
     particles_c[i].move();
     particles_c[i].display(radius);
     particles_c[i].checkEdge();
-  } 
-  
-  firstLoop = false;
+  }
 }
 
-function drawGUI(){
-  stroke(132,164,246, 255);
+function drawGUI() {
+  fill(textColor);
+  stroke(textColor);
   text("Where do you feel most relaxed?", windowWidth/2, windowHeight/4);
 }
 
-// Particle Class
-function Particle(x, y){
+function Particle(x, y) {
   this.dir = createVector(0, 0);
   this.vel = createVector(0, 0);
   this.pos = createVector(x, y);
   this.speed = 0.4;
 
-  this.move = function(){
+  this.move = function() {
     var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*TWO_PI*noiseScale;
     this.dir.x = cos(angle);
     this.dir.y = sin(angle);
     this.vel = this.dir.copy();
     this.vel.mult(this.speed);
     this.pos.add(this.vel);
-  }
+  };
 
-  this.checkEdge = function(){
+  this.checkEdge = function() {
     if(this.pos.x > width || this.pos.x < 0 || this.pos.y > height || this.pos.y < 0){
       this.pos.x = random(50, width);
       this.pos.y = random(50, height);
     }
-  }
+  };
 
   this.display = function(r){
     ellipse(this.pos.x, this.pos.y, r, r);
