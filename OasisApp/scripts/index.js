c6b83ccddd8f4355f322dbc5105079bb0a7a00f1
Cimo02 <tjc3255@g.rit.edu>
2020-03-05 10:01:28 -0500

    Added hovering to question options

--- OasisApp/scripts/index.js
+++ OasisApp/scripts/index.js
@@ -23,41 +23,73 @@ function answerQuestion(question) {
         case 1:
             window.location.href = "question2.html";
             console.log("after Q1: " + answers);
+            console.log(currentAnswer + " in answered...");
             break;
         case 2:
             window.location.href = "question3.html";
             console.log("after Q2: " + answers);
+            console.log(currentAnswer + " in answered...");
             break;
         case 3:
             window.location.href = "app.html";
             console.log("after Q3: " + answers);
+            console.log(currentAnswer + " in answered...");
             break;
     }
+
+    
 }
 
+// element is the option's outer circle
 function optionSelected(el) {
-    // reset other option elements
-    var options = document.getElementsByClassName("optionInner");
-    for(var i = 0; i < options.length; i++){
-        options[i].classList.remove("selected");
-        options[i].classList.remove("notSelected");
-        options[i].classList.add("notSelected");
+    // reset other option elements to notSelected
+    var innerOptions = document.getElementsByClassName("optionInner");
+    var outerOptions = document.getElementsByClassName("optionOuter");
+    for(var i = 0; i < innerOptions.length; i++){
+        innerOptions[i].classList.remove("selected");
+    }
+    for(var i = 0; i < outerOptions.length; i++){
+        outerOptions[i].classList.remove("selectedOuter");
     }
 
-    // set the correct element class
-    el.classList.remove("notSelected");
-    el.classList.add("selected");
-
-    // set currentAnswer variable
-    if (el.classList.contains("oneOptionInner")) {
+    // el could be the outer or inner circle depending on where the user clicked
+    if (el.classList.contains("oneOptionOuter") || el.classList.contains("oneOptionInner")) {
+        innerOptions[0].classList.add("selected");
         currentAnswer = 0;
-    } else if (el.classList.contains("twoOptionInner")) {
+    } else if (el.classList.contains("twoOptionOuter") || el.classList.contains("twoOptionInner")) {
+        innerOptions[1].classList.add("selected");
         currentAnswer = 1;
     } else {
+        innerOptions[2].classList.add("selected");
         currentAnswer = 2;
     }
     // DEBUG answer
-    console.log(currentAnswer + " selected...");
+    console.log(currentAnswer + " in selected...");
+}
+
+function optionMouseOver(el) {
+    // remove hovered class from every button that has it 
+    var options = document.getElementsByClassName("optionInner");
+    for(var i = 0; i < options.length; i++){
+        options[i].classList.remove("hovered");
+    }
+
+    // set currentAnswer variable
+    if (el.classList.contains("oneOptionOuter") || el.classList.contains("oneOptionInner")) {
+        options[0].classList.add("hovered");
+    } else if (el.classList.contains("twoOptionOuter") || el.classList.contains("twoOptionInner")) {
+        options[1].classList.add("hovered");
+    } else {
+        options[2].classList.add("hovered");
+    }
+}
+
+function optionMouseOut() {
+    // remove hovered from all buttons 
+    var options = document.getElementsByClassName("optionInner");
+    for(var i = 0; i < options.length; i++){
+        options[i].classList.remove("hovered");
+    }
 }
 
 function back(question) {
