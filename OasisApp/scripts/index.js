1444669ea9415db72565f2c8e39564a3428d8f9e
Andrew Hurley <hurleymandrew@gmail.com>
2020-04-17 19:08:44 -0400

    more responsiveness

--- OasisApp/scripts/index.js
+++ OasisApp/scripts/index.js
@@ -1,48 +1,49 @@
 function restart() {
   document.getElementById("mainMenuFade").classList.add("wrapperFade");
-  setTimeout(function() {
+  setTimeout(function () {
     window.location.href = "index.html";
   }, 1000);
 }
 
-
-
-
-
-
-
-
-
-
-
 let answers;
 let currentAnswer;
 
-let flow = ['index.html', 'question1.html', 'question2.html', 'question3.html', 'calibrationPage.html', 'app.html'];
+let flow = [
+  "index.html",
+  "question1.html",
+  "question2.html",
+  "question3.html",
+  "calibrationPage.html",
+  "app.html",
+];
 let url = window.location.pathname;
-let fileName = url.substring(url.lastIndexOf('/') + 1);
+4;
+let fileName = url.substring(url.lastIndexOf("/") + 1);
 
 // clear answers when begin button is clicked
 const beginQuestions = () => {
   answers = [null, null, null];
-  localStorage.setItem('answers', JSON.stringify(answers));
+  localStorage.setItem("answers", JSON.stringify(answers));
 
   // page transition
-  document.querySelector('.wrapper').classList.add('fadeOut');
-  setTimeout(() => window.location.href = flow[flow.indexOf(fileName) + 1], 1000);
+  document.querySelector(".wrapper").classList.add("fadeOut");
+  setTimeout(
+    () => (window.location.href = flow[flow.indexOf(fileName) + 1]),
+    1000
+  );
 };
 
 // give option buttons selection styling on click
 window.onload = () => {
-  let optionButtons = document.querySelectorAll('.buttonContainer');
+  let optionButtons = document.querySelectorAll(".buttonContainer");
 
   for (let button of optionButtons) {
-    button.addEventListener('click', function () {
+    button.addEventListener("click", function () {
       for (let b of optionButtons) {
-        b.querySelector('.buttonInner').classList.remove('selected');
+        b.querySelector(".buttonInner").classList.remove("selected");
       }
 
-      button.querySelector('.buttonInner').classList.add('selected');
+      button.querySelector(".buttonInner").classList.add("selected");
       currentAnswer = parseInt(button.id);
     });
   }
@@ -50,44 +51,44 @@ window.onload = () => {
 
 // page transition
 const fadeAnimations = () => {
-  let images = document.querySelectorAll('.optionImage');
+  let images = document.querySelectorAll(".optionImage");
   for (let i of images) {
     const listener = () => {
-      i.removeEventListener('animationend', listener);
+      i.removeEventListener("animationend", listener);
     };
 
-    i.addEventListener('animationend', listener);
+    i.addEventListener("animationend", listener);
 
-    i.classList.add('imageWipeOut');
+    i.classList.add("imageWipeOut");
   }
 
   {
-    let prevBreadcrumb = document.querySelector('.navCurrentBreadcrumb');
+    let prevBreadcrumb = document.querySelector(".navCurrentBreadcrumb");
     const listener = () => {
-      prevBreadcrumb.removeEventListener('animationend', listener);
+      prevBreadcrumb.removeEventListener("animationend", listener);
     };
-    prevBreadcrumb.addEventListener('animationend', listener);
-    prevBreadcrumb.classList.remove('navCurrentBreadcrumb');
-    prevBreadcrumb.classList.add('navPreviousBreadcrumb');
+    prevBreadcrumb.addEventListener("animationend", listener);
+    prevBreadcrumb.classList.remove("navCurrentBreadcrumb");
+    prevBreadcrumb.classList.add("navPreviousBreadcrumb");
   }
 
   {
-    let questionText = document.querySelector('.questionText');
+    let questionText = document.querySelector(".questionText");
     const listener = () => {
-      questionText.removeEventListener('animationend', listener);
+      questionText.removeEventListener("animationend", listener);
     };
-    questionText.addEventListener('animationend', listener);
-    questionText.classList.add('questionTextFadeOut');
+    questionText.addEventListener("animationend", listener);
+    questionText.classList.add("questionTextFadeOut");
   }
 
   {
-    let optionText = document.querySelectorAll('.optionText');
+    let optionText = document.querySelectorAll(".optionText");
     for (let text of optionText) {
       const listener = () => {
-        text.removeEventListener('animationend', listener);
+        text.removeEventListener("animationend", listener);
       };
-      text.addEventListener('animationend', listener);
-      text.classList.add('optionTextFadeOut');
+      text.addEventListener("animationend", listener);
+      text.classList.add("optionTextFadeOut");
     }
   }
 };
@@ -95,7 +96,10 @@ const fadeAnimations = () => {
 // back to previous page
 const back = () => {
   fadeAnimations();
-  setTimeout(() => window.location.href = flow[flow.indexOf(fileName) - 1], 2000);
+  setTimeout(
+    () => (window.location.href = flow[flow.indexOf(fileName) - 1]),
+    2000
+  );
 };
 
 // answer current question
@@ -104,10 +108,14 @@ const answerQuestion = () => {
     return;
   }
 
-  answers = JSON.parse(localStorage.getItem('answers'));
+  answers = JSON.parse(localStorage.getItem("answers"));
   answers[document.body.className - 1] = currentAnswer;
-  localStorage.setItem('answers', JSON.stringify(answers));
+  localStorage.setItem("answers", JSON.stringify(answers));
 
   fadeAnimations();
-  setTimeout(() => window.location.href = flow[flow.indexOf(fileName) + 1], 2000);
+  setTimeout(
+    () => (window.location.href = flow[flow.indexOf(fileName) + 1]),
+    2000
+  );
 };
+3;
